93185b9cf6a0336344db9cf8b3284981d9a036b1
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2024-05-07 20:00:51 +0100

    Feat: Profile page started

--- resources/js/Pages/Agent/Index.vue
+++ resources/js/Pages/Agent/Index.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+</script>
+
 <template>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio suscipit sequi quibusdam inventore repellat facere obcaecati porro nemo numquam odit eveniet nam nobis quos enim ipsam, excepturi minima dolore minus!
+    <AuthenticatedLayout>
+        <div class="rounded h-96 bg-white">
+            <div class="h-64 overflow-hidden">
+                <img src="/images/cover.jpg" alt="" srcset="" class="h-100 w-full rounded-l rounded-r">
+            </div>
+            <div class="p-4">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio suscipit sequi quibusdam inventore repellat facere obcaecati porro nemo numquam odit eveniet nam nobis quos enim ipsam, excepturi minima dolore minus!
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
